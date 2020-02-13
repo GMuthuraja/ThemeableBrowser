3b35c5ae9fe5af04a084a58e059e2c34855dca00
Muthuraja <muthuraja.ganesan@raqmiyat.com>
2020-02-13 19:29:19 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ThemeableBrowser } from '@ionic-native/themeable-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +16,10 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
+    ThemeableBrowser,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,85 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ThemeableBrowser, ThemeableBrowserOptions, ThemeableBrowserObject } from '@ionic-native/themeable-browser/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  safeUrl: any;
+  CheckinURL: string = "https://checkin.si.amadeus.net/static/PRD/SV/#/identification";
+  StaffURL: string = "https://svbooking.saudiairlines.com:11011/apps/corp/TravelPortalM/StaffBookingM.aspx";
+  OtherBookingURL: string = "https://svbooking.saudiairlines.com:11011/apps/corp/TravelPortalM/OAB.aspx";
+  options: ThemeableBrowserOptions;
+  browser: ThemeableBrowserObject;
 
+  constructor(
+    private sanitizer: DomSanitizer,
+    private iab: InAppBrowser,
+    private themeableBrowser: ThemeableBrowser) {
+
+    this.options = {
+      toolbar: {
+        height: 44,
+        color: '#3880ff'
+      },
+      title: {
+        color: '#ffffff',
+        showPageTitle: true,
+        staticText: 'Themeable Browser'
+      },
+      backButton: {
+        image: '../../assets/icon/favicon.png',
+        align: 'left',
+        event: 'closePressed'
+      },
+      closeButton: {
+        image: '../../assets/icon/close.svg',
+        align: 'right',
+        event: 'closePressed'
+      }
+    }
+  }
+
+  openCheckinURL() {
+    //this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.CheckinURL);
+    this.iab.create(this.CheckinURL, '_blank');
+  }
+
+  openStaffURL() {
+    //this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.StaffURL);
+    this.iab.create(this.StaffURL, '_blank');
+  }
+
+  openOtherURL() {
+    //this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.OtherBookingURL);
+    this.iab.create(this.OtherBookingURL, '_blank');
+  }
+
+  openTBCheckinURL() {
+    this.browser = this.themeableBrowser.create(this.CheckinURL, '_blank', this.options);
+
+    // this.browser.on('closePressed').subscribe(data => {
+    //   this.browser.close();
+    // });
+  }
+
+  openTBStaffURL() {
+    this.browser = this.themeableBrowser.create(this.StaffURL, '_blank', this.options);
+    // this.browser.on('closePressed').subscribe(data => {
+    //   this.browser.close();
+    // });
+  }
+
+  openTBOtherURL() {
+    this.browser = this.themeableBrowser.create(this.OtherBookingURL, '_blank', this.options);
+    // this.browser.on('closePressed').subscribe(data => {
+    //   this.browser.close();
+    // });
+  }
 }
